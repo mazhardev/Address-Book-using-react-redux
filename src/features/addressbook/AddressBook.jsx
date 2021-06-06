100063faadba54bd0737623135e4f9ed8206f341
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2021-06-06 13:02:50 +0500

    change class compenets to functional to use hooks

--- src/features/addressbook/AddressBook.jsx
+++ src/features/addressbook/AddressBook.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Menu, Header, Input, Icon } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { Button } from "semantic-ui-react";
@@ -8,84 +8,67 @@ const mapState = state => ({
   addresses: state.test
 });
 
+const AddressBook = ({ addresses }) => {
+  const [Addresses, setAddresses] = useState([])
+  const [searchField, setSearchField] = useState("")
 
-class AddressBook extends Component {
-  constructor(props) {
-    super(props);
-    let sortedAddrBook = this.props.addresses.sort(function (a, b) {
-      if (a.fName < b.fName) {
-        return -1;
-      }
-      if (a.fName > b.fName) {
-        return 1;
-      }
-      return 0;
-    });
-    this.state = {
-      Addresses: sortedAddrBook,
-      searchField: ""
-    };
-  }
+  useEffect(() => {
+    setAddresses(addresses)
+  }, [addresses])
+  const onSearchChange = e =>
+    setSearchField(e.target.value);
 
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
-  };
-  componentWillReceiveProps(newProps) {
-    this.setState({
-      Addresses: newProps.addresses.sort(function (a, b) {
-        if (a.fName < b.fName) {
-          return -1;
-        }
-        if (a.fName > b.fName) {
-          return 1;
-        }
-        return 0;
-      })
-    })
-  }
+  let sortedAddrBook = Addresses.sort(function (a, b) {
+    if (a.fName < b.fName) {
+      return -1;
+    }
+    if (a.fName > b.fName) {
+      return 1;
+    }
+    return 0;
+  });
+  const filteredAddresses = sortedAddrBook.filter(Address => {
+    return Address.fName.toLowerCase().includes(searchField.toLowerCase());
+  });
 
-  render() {
-    const { Addresses, searchField } = this.state;
-    const filteredAddresses = Addresses.filter(Address => {
-      return Address.fName.toLowerCase().includes(searchField.toLowerCase());
-    });
-    return (
-      <Menu vertical style={{ minWidth: "100%" }}>
-        <Header
-          icon="user"
-          attached
-          inverted
-          color="grey"
-          content="My Address Book"
-        />
-        <Button
+
+  return (
+    <Menu vertical style={{ minWidth: "100%" }}>
+      <Header
+        icon="user"
+        attached
+        inverted
+        color="grey"
+        content="My Address Book"
+      />
+      <Button
+        as={Link}
+        to="/AddressBookForm"
+        color="green"
+        floated="right"
+        content="Add"
+      />
+      <Input
+        onChange={onSearchChange}
+        fluid
+        icon="search"
+        placeholder="Search..."
+        width="120px"
+      />
+      {filteredAddresses.map((address, index) => (
+        <Menu.Item
+          key={index}
           as={Link}
-          to="/AddressBookForm"
-          color="green"
-          floated="right"
-          content="Add"
-        />
-        <Input
-          onChange={this.onSearchChange}
-          fluid
-          icon="search"
-          placeholder="Search..."
-          width="120px"
-        />
-        {filteredAddresses.map((address, index) => (
-          <Menu.Item
-            key={index}
-            as={Link}
-            to={`/AddressBookForm/${address.id}`}
-          >
-            {address.fName + "  " + address.lName}
-            <Icon name='edit' />
-          </Menu.Item>
-        ))}
-      </Menu>
-    );
-  }
+          to={`/AddressBookForm/${address.id}`}
+        >
+          {address.fName + "  " + address.lName}
+          <Icon name='edit' />
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
 }
+
 
 export default connect(
   mapState,
